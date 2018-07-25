5bab873e9484d5d9c8b9d5c3c4e942eb37683f80
Wang YiHong <wangyihong1997@outlook.com>
2018-07-25 11:29:41 +0800

    测试2

--- test.py
+++ test.py
@@ -3,5 +3,3 @@ str1 = "%s,'%s','%s','%s','%s',%s,%s,%s,'%s','%s','%s',%s,%s,%s,%s"
 str1 = str1 % (other_info[0],other_info[1],other_info[2],other_info[3],other_info[4],other_info[5],other_info[6],other_info[7],other_info[8],other_info[9],other_info[10],other_info[11],other_info[12],other_info[13],other_info[14])
 print(str1)
 
-
-123242152121
