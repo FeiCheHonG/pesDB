9605e14175f68c1fb1c910d2d5b8243a0d84c9ee
Wang YiHong <wangyihong1997@outlook.com>
2018-07-25 11:25:28 +0800

    测试修改

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ other_info = ['7', 'MD WHITE', 'Spanish League', 'PORTUGAL', 'Europe', '185', '8
 str1 = "%s,'%s','%s','%s','%s',%s,%s,%s,'%s','%s','%s',%s,%s,%s,%s"
 str1 = str1 % (other_info[0],other_info[1],other_info[2],other_info[3],other_info[4],other_info[5],other_info[6],other_info[7],other_info[8],other_info[9],other_info[10],other_info[11],other_info[12],other_info[13],other_info[14])
 print(str1)
+
+
+123242152121
